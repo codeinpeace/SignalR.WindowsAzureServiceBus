36800f3c4f096420e4a8757fd9911090f3d41c17
David Fowler <davidfowl@gmail.com>
2012-02-13 01:27:44 -0800

    Added dependency resolver extensions for configuring service bus.

---
+++ SignalR.WindowsAzureServiceBus/DependencyResolverExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using SignalR.Infrastructure;
+using SignalR.MessageBus;
+
+namespace SignalR.WindowsAzureServiceBus
+{
+    public static class DependencyResolverExtensions
+    {
+
+        public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
+                                                                    string serviceBusNamespace,
+                                                                    string serviceBusAccount,
+                                                                    string serviceBusAccountKey,
+                                                                    string topicPathPrefix,
+                                                                    int numberOfTopics)
+        {
+            var instanceId = Environment.MachineName + "_" + GetRoleInstanceNumber().ToString();
+
+            return UseWindowsAzureServiceBus(resolver, 
+                                             serviceBusNamespace, 
+                                             serviceBusAccount, 
+                                             serviceBusAccountKey, 
+                                             topicPathPrefix, 
+                                             numberOfTopics,
+                                             instanceId);
+        }
+
+        public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
+                                                                    string serviceBusNamespace,
+                                                                    string serviceBusAccount,
+                                                                    string serviceBusAccountKey,
+                                                                    string topicPathPrefix,
+                                                                    int numberOfTopics,
+                                                                    string instanceId)
+        {
+            var bus = new Lazy<ServiceBusMessageBus>(() => new ServiceBusMessageBus(topicPathPrefix, 
+                                                                                    numberOfTopics, 
+                                                                                    serviceBusNamespace, 
+                                                                                    serviceBusAccount, 
+                                                                                    serviceBusAccountKey, 
+                                                                                    instanceId));
+            resolver.Register(typeof(IMessageBus), () => bus.Value);
+            return resolver;
+        }
+
+        private static int GetRoleInstanceNumber()
+        {
+            var roleInstanceId = RoleEnvironment.CurrentRoleInstance.Id;
+            var li1 = roleInstanceId.LastIndexOf(".");
+            var li2 = roleInstanceId.LastIndexOf("_");
+            var roleInstanceNo = roleInstanceId.Substring(Math.Max(li1, li2) + 1);
+            return Int32.Parse(roleInstanceNo);
+        }
+    }
+}
